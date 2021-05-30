7d6fd83c8b8706620a45a2c072ffae5021cc6c93
Cicrye <35354666+Cicrye@users.noreply.github.com>
2021-05-30 15:16:06 +0200

    new filter for name

--- resources/js/components/novabalanceops.vue
+++ resources/js/components/novabalanceops.vue
@@ -73,6 +73,31 @@
                 </div>
             </v-menu>
         </template>
+        <template v-slot:header.author="{header}">
+            {{header.text}}
+            <v-menu offset-y :close-on-content-click="false">
+                <template v-slot:activator="{ on, attrs }">
+                    <v-btn icon v-bind="attrs" v-on="on">
+                        <span class="material-icons">
+                            filter_alt
+                        </span>
+                    </v-btn>
+                </template>
+                <div style="background-color: white; width: 280px">
+                    <v-text-field v-model="responsibleName"
+                    class="pa-4"
+                    type="text"
+                    label="Enter the author name"
+                    ></v-text-field>
+                    <v-btn @click="responsibleName = ''"
+                    small
+                    text
+                    color="primary"
+                    class="ml-2 mb-2"
+                    >Clear</v-btn>
+                </div>
+            </v-menu>
+        </template>
         </v-data-table>
     </div>
 </v-app>
@@ -85,6 +110,7 @@
             search: '',
             authorName: '',
             commandIssued: '',
+            responsibleName: '',
             items: [],
             weeks: [
                 { id: 1, week: "All weeks"},
@@ -124,6 +150,9 @@
         filterCommand(item) {
             return item.command.toLowerCase().includes(this.commandIssued.toLowerCase());
         },
+        filterName(item) {
+            return item.command.toLowerCase().includes(this.responsibleName.toLowerCase());
+        },
     },
     computed: {
         filteredItems() {
@@ -134,8 +163,12 @@
 
             if (this.commandIssued) {
                 conditions.push(this.filterCommand);
-
             }
+
+            if (this.responsibleName) {
+                conditions.push(this.filterCommand);
+            }
+
             if (conditions.length > 0) {
                 return this.items.filter((item) => {
                     return conditions.every((condition) => {
