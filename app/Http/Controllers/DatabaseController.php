d52472bef27a29d99e569be5958bec9ba968b459
Cicrye <35354666+Cicrye@users.noreply.github.com>
2021-07-20 08:13:49 +0200

    fix missing payments

--- app/Http/Controllers/DatabaseController.php
+++ app/Http/Controllers/DatabaseController.php
@@ -408,13 +408,36 @@ class DatabaseController extends Controller
     public function showMissingPayments($faction) {
         switch ($faction) {
             case 'alliance':
-                $items = DB::select("SELECT ov_creds.booster, ov_creds.tot_balance-ov_creds.cur_balance AS total, COALESCE(SUM(payments.amount),0) AS paid, (ov_creds.tot_balance-COALESCE(SUM(payments.amount),0))-ov_creds.cur_balance AS missing FROM ov_creds LEFT JOIN payments ON payments.booster = ov_creds.booster WHERE payments.booster LIKE '%[A]' GROUP BY 1 ORDER BY 1 ASC");
+                $items = DB::select("SELECT ov_creds.booster, ov_creds.tot_balance-ov_creds.cur_balance-ov_creds.pre_balance AS total,
+                COALESCE(SUM(payments.amount),0)-COALESCE(paymentsv2.paid,0) AS paid,
+                ov_creds.tot_balance-ov_creds.cur_balance-ov_creds.pre_balance-(COALESCE(SUM(payments.amount),0)-COALESCE(paymentsv2.paid,0)) AS missing
+                FROM ov_creds
+                LEFT JOIN payments ON payments.booster = ov_creds.booster
+                LEFT JOIN paymentsv2 ON paymentsv2.booster = ov_creds.booster
+                WHERE payments.booster LIKE '%[A]'
+                GROUP BY 1
+                ORDER BY 1 ASC");
                 break;
             case 'horde':
-                $items = DB::select("SELECT ov_creds.booster, ov_creds.tot_balance-ov_creds.cur_balance AS total, COALESCE(SUM(payments.amount),0) AS paid, (ov_creds.tot_balance-COALESCE(SUM(payments.amount),0))-ov_creds.cur_balance AS missing FROM ov_creds LEFT JOIN payments ON payments.booster = ov_creds.booster WHERE payments.booster LIKE '%[H]' GROUP BY 1 ORDER BY 1 ASC");
+                $items = DB::select("SELECT ov_creds.booster, ov_creds.tot_balance-ov_creds.cur_balance-ov_creds.pre_balance AS total,
+                COALESCE(SUM(payments.amount),0)-COALESCE(paymentsv2.paid,0) AS paid,
+                ov_creds.tot_balance-ov_creds.cur_balance-ov_creds.pre_balance-(COALESCE(SUM(payments.amount),0)-COALESCE(paymentsv2.paid,0)) AS missing
+                FROM ov_creds
+                LEFT JOIN payments ON payments.booster = ov_creds.booster
+                LEFT JOIN paymentsv2 ON paymentsv2.booster = ov_creds.booster
+                WHERE payments.booster LIKE '%[H]'
+                GROUP BY 1
+                ORDER BY 1 ASC");
                 break;
             case 'all':
-                $items = DB::select("SELECT ov_creds.booster, ov_creds.tot_balance-ov_creds.cur_balance AS total, COALESCE(SUM(payments.amount),0) AS paid, (ov_creds.tot_balance-COALESCE(SUM(payments.amount),0))-ov_creds.cur_balance AS missing FROM ov_creds LEFT JOIN payments ON payments.booster = ov_creds.booster GROUP BY 1 ORDER BY 1 ASC");
+                $items = DB::select("SELECT ov_creds.booster, ov_creds.tot_balance-ov_creds.cur_balance-ov_creds.pre_balance AS total,
+                COALESCE(SUM(payments.amount),0)-COALESCE(paymentsv2.paid,0) AS paid,
+                ov_creds.tot_balance-ov_creds.cur_balance-ov_creds.pre_balance-(COALESCE(SUM(payments.amount),0)-COALESCE(paymentsv2.paid,0)) AS missing
+                FROM ov_creds
+                LEFT JOIN payments ON payments.booster = ov_creds.booster
+                LEFT JOIN paymentsv2 ON paymentsv2.booster = ov_creds.booster
+                GROUP BY 1
+                ORDER BY 1 ASC");
                 break;
         }
 
