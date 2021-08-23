cf4fb6d466d99a769b2fb98bfe1b58acd0dfe7f1
Cicrye <35354666+Cicrye@users.noreply.github.com>
2021-08-24 01:10:30 +0200

    fixed realm with '

--- app/Http/Controllers/DatabaseController.php
+++ app/Http/Controllers/DatabaseController.php
@@ -950,7 +950,7 @@ class DatabaseController extends Controller
     public function getPaymentsRealm($realm)
     {
         $items = DB::select("SELECT SUM(amount) as total FROM payments
-        WHERE booster LIKE '%".$realm."' AND paymentdate BETWEEN (SELECT cur1 FROM `variables` WHERE id = 1) AND
+        WHERE booster LIKE '%".str_replace("'", "\\'", $realm)."' AND paymentdate BETWEEN (SELECT cur1 FROM `variables` WHERE id = 1) AND
         (SELECT cur2 FROM `variables` WHERE id = 1);");
 
         if ($items[0]->total == null) {
