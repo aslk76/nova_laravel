1408790216b6a8a816733988a82d2d6c4cc209b0
Cicrye <35354666+Cicrye@users.noreply.github.com>
2021-09-22 01:23:05 +0200

    balanceops changes

--- app/Http/Controllers/DatabaseController.php
+++ app/Http/Controllers/DatabaseController.php
@@ -279,7 +279,7 @@ class DatabaseController extends Controller
     public function getBalanceOps($weekid)
     {
         if ($weekid == 1) {
-            $items = Balanceops::whereNull('deleted_at')->get();
+            $items = Balanceops::whereNull('deleted_at')->where('command', '<>', 'Casino')->where('command', '<>', 'Lottery')->get();
             return $items;
         }
         if ($weekid == 2) {
