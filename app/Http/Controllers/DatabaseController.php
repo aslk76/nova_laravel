8d93e9c5203cb136171aee1bceb83fecdd29f57c
Cicrye <35354666+Cicrye@users.noreply.github.com>
2021-09-22 01:25:19 +0200

    remove dd

--- app/Http/Controllers/DatabaseController.php
+++ app/Http/Controllers/DatabaseController.php
@@ -280,7 +280,6 @@ class DatabaseController extends Controller
     {
         if ($weekid == 1) {
             $items = Balanceops::whereNull('deleted_at')->where('command', '<>', 'Casino')->where('command', '<>', 'Lottery')->get();
-            dd($items);
             return $items;
         }
         if ($weekid == 2) {
