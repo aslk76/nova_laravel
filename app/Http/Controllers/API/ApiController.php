2dcf15452683d31843e9aee2bdd8816d9c101049
Cicrye <35354666+Cicrye@users.noreply.github.com>
2021-11-04 21:47:51 +0100

    controller

--- app/Http/Controllers/API/ApiController.php
+++ app/Http/Controllers/API/ApiController.php
@@ -8,6 +8,8 @@ use App\RaidCollecting;
 use Illuminate\Support\Facades\DB;
 use Exception;
 use Illuminate\Support\Facades\Log;
+use App\Http\Controllers\Controller as Controller;
+
 class ApiController extends Controller
 {
     public function sendRaidToDB(Request $request) {
