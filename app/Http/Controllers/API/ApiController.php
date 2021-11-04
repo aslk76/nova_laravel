c61f28e4a14513f70aff0a7fa7fac8d1db44d6c2
Cicrye <35354666+Cicrye@users.noreply.github.com>
2021-11-04 21:41:50 +0100

    rename

--- app/Http/Controllers/API/ApiController.php
+++ app/Http/Controllers/API/ApiController.php
@@ -8,7 +8,7 @@ use App\RaidCollecting;
 use Illuminate\Support\Facades\DB;
 use Exception;
 use Illuminate\Support\Facades\Log;
-class DatabaseController extends Controller
+class ApiController extends Controller
 {
     public function sendRaidToDB(Request $request) {
         dd($request);
