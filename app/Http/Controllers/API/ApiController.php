0f260b74125d6037d15ef9820825b0d5ce328a3d
Cicrye <35354666+Cicrye@users.noreply.github.com>
2021-11-04 22:30:03 +0100

    test with the sql

--- app/Http/Controllers/API/ApiController.php
+++ app/Http/Controllers/API/ApiController.php
@@ -13,6 +13,13 @@ use App\Http\Controllers\Controller as Controller;
 class ApiController extends Controller
 {
     public function sendRaidToDB(Request $request) {
-        dd($request);
+        //dd($request);
+        $values = DB::select("SELECT raid_book.advertiser_name AS `name`, realms_paid.name AS paidin, raid_book.paid AS amount, realms_adv.name AS advertiser_realm
+        FROM `nova_applications`.raid_book
+        LEFT JOIN `nova_applications`.realms realms_paid ON raid_book.paid_realm_id = realms_paid.id
+        LEFT JOIN `nova_applications`.realms realms_adv ON raid_book.adv_realm_id  = realms_adv.id
+        WHERE raid_id = " + $request->id);
+
+        dd($values);
     }
 }
