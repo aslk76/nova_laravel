26ee550e1279c4ed20b904695df453b1e50babe3
Cicrye <35354666+Cicrye@users.noreply.github.com>
2021-11-05 11:29:46 +0100

    fix

--- app/Http/Controllers/API/ApiController.php
+++ app/Http/Controllers/API/ApiController.php
@@ -54,11 +54,9 @@ class ApiController extends Controller
                 WHERE raid.id = ".$request->id))->first();
                 $boosters = str_replace(["[","]"],"",$values->boosters);
                 $boosters = explode(",", $boosters);
-                dd($boosters);
                 foreach ($boosters as $booster) {
-                    $fullname = collect(\DB::select("SELECT name from users where id = ".$booster))->first();
+                    $fullname = collect(\DB::select("SELECT name from `nova_applications`.users where id = ".$booster))->first();
                     $splitname = explode("-", $fullname->name);
-                    dd($splitname);
                     $balance = new RaidBalance;
                     $balance->import_date = $date;
                     $balance->name = $splitname[0];
