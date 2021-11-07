20e1fc8f4d0ef046f232f676fa5a87df537b83fd
Cicrye <35354666+Cicrye@users.noreply.github.com>
2021-11-07 16:04:28 +0100

    test

--- app/Http/Controllers/API/ApiController.php
+++ app/Http/Controllers/API/ApiController.php
@@ -49,6 +49,7 @@ class ApiController extends Controller
                     $advpot = $value->amount*0.10;
                 } else {
                     $roles = str_replace(["[\"","\"]"],"",$fullname->discord_rank);
+                    $roles = str_replace(["\"","\""],"",$fullname->discord_rank);
                     $roles = explode(",", $roles);
                     dd($roles, array_search("Hotshot Advertiser H", $roles));
                     if ($faction->faction == "horde" && array_search('Hotshot Advertiser H', $roles) > 0) {
