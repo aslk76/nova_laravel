2d07b624a533f8e5789aa304aeadd29b62ef453d
Cicrye <35354666+Cicrye@users.noreply.github.com>
2021-11-14 22:33:04 +0100

    fixed

--- app/Http/Controllers/API/ApiController.php
+++ app/Http/Controllers/API/ApiController.php
@@ -101,10 +101,7 @@ class ApiController extends Controller
 
                     DB::transaction(function () use ($date, $splitname, $cut) {
                         DB::statement("INSERT INTO `raid_balance` (`import_date`,`name`,`realm`,`amount`)
-                        VALUES ('".$date."',
-                        ".$splitname[0]."',
-                        '".$splitname[1]."',
-                        ".$cut.")
+                        VALUES ('".$date."','".$splitname[0]."','".$splitname[1]."',".$cut.")
                         ON DUPLICATE KEY UPDATE
                         `import_date`=VALUES(`import_date`), `amount`=`amount`+VALUES(`amount`);");
                     }, 5);
