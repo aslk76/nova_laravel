efd2afe94304e32fddcb20ee4591eadf05545dec
Cicrye <35354666+Cicrye@users.noreply.github.com>
2021-11-17 14:12:22 +0100

    new api edit booster cut

--- app/Http/Controllers/API/ApiController.php
+++ app/Http/Controllers/API/ApiController.php
@@ -111,50 +111,30 @@ class ApiController extends Controller
                     }, 5);
                 }
             }
-            // $values = collect(\DB::select("SELECT leader_id, guild_id, boosters, rl_cut, booster_cut from `nova_applications`.raid where id = " . $request->id))->first();
-            // if (!is_null($values->guild_id)) {
-            //     $grep = collect(\DB::select("SELECT pay_character from `nova_applications`.guilds where id = ".$values->guild_id))->first();
-            //     $grep = explode("-", $grep->pay_character);
-            //     $rlpot = $values->booster_cut + $values->rl_cut;
-            //     DB::transaction(function () use ($date, $splitname, $rlpot) {
-            //         DB::statement("INSERT INTO `raid_balance` (`import_date`,`name`,`realm`,`amount`)
-            //         VALUES ('".$date."', '".$grep[0]."', '".$grep[1]."', ".$rlpot.")
-            //         ON DUPLICATE KEY UPDATE
-            //         `import_date`=VALUES(`import_date`), `amount`=`amount`+VALUES(`amount`);");
-            //     }, 5);
-            // } else {
-            //     $raidleader = collect(\DB::select("SELECT leader_name.name
-            //     FROM `nova_applications`.raid
-            //     LEFT JOIN `nova_applications`.users leader_name ON raid.leader_id = leader_name.id
-            //     WHERE raid.id = ".$request->id))->first();
-            //     $boosters = str_replace(["[","]"],"",$values->boosters);
-            //     $boosters = explode(",", $boosters);
-            //     // $data = array();
-            //     foreach ($boosters as $booster) {
-            //         $fullname = collect(\DB::select("SELECT name, staff_name from `nova_applications`.users where id = ".$booster))->first();
-            //         if (!is_null($fullname->staff_name)) {
-            //             $splitname = explode("-", $fullname->staff_name);
-            //         } else {
-            //             $splitname = explode("-", $fullname->name);
-            //         }
-            //         if ($raidleader->name == $splitname[0]."-".$splitname[1]) {
-            //             $rlpot = $values->booster_cut + $values->rl_cut;
-            //             DB::transaction(function () use ($date, $splitname, $rlpot) {
-            //                 DB::statement("INSERT INTO `raid_balance` (`import_date`,`name`,`realm`,`amount`)
-            //                 VALUES ('".$date."', '".$splitname[0]."', '".$splitname[1]."', ".$rlpot.")
-            //                 ON DUPLICATE KEY UPDATE
-            //                 `import_date`=VALUES(`import_date`), `amount`=`amount`+VALUES(`amount`);");
-            //             }, 5);
-            //         } else {
-            //             DB::transaction(function () use ($date, $splitname, $values) {
-            //                 DB::statement("INSERT INTO `raid_balance` (`import_date`,`name`,`realm`,`amount`)
-            //                 VALUES ('".$date."', '".$splitname[0]."', '".$splitname[1]."', ".$values->booster_cut.")
-            //                 ON DUPLICATE KEY UPDATE
-            //                 `import_date`=VALUES(`import_date`), `amount`=`amount`+VALUES(`amount`);");
-            //             }, 5);
-            //         }
-            //     }
-            // }
+            return response()->json('OK');
+        } catch (Exception $e) {
+            Log::error($e);
+            return response()->json('KO');
+        }
+    }
+
+    public function editBoosterCut(Request $request) {
+        try {
+            $amountChange = $request->old_pot - $request->new_pot
+
+            $raidTime = strtotime($request->raid_time);
+            if (date('D', $raidTime) == 'Tue') {
+                $date = date('Y-m-d', $raidTime);
+            } else {
+                $timestamp = strtotime('next tuesday', $raidTime);
+                $date = date('Y-m-d', $timestamp);
+            }
+
+            $fullname = explode("-", $request->user_name);
+
+            DB::transaction(function () use ($date, $fullname, $amountChange) {
+                DB::statement("UPDATE raid_balance SET amount = amount - ".$amountChange." WHERE import_date = ".$date." AND NAME = '".$fullname[0]."' AND realm = '".$fullname[1]."'");
+            }, 5);
 
             return response()->json('OK');
         } catch (Exception $e) {
