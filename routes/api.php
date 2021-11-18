1687862e949cb084f93e000c87922087926fc36a
Cicrye <35354666+Cicrye@users.noreply.github.com>
2021-11-18 22:11:25 +0100

    no prefix

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group(['prefix' => 'v1'], function () {
-    Route::post('/sendRaidToDB', 'ApiController@sendRaidToDB');
-    Route::post('/editBoosterCut', 'ApiController@editBoosterCut');
-});
+Route::post('/sendRaidToDB', 'ApiController@sendRaidToDB');
+Route::post('/editBoosterCut', 'ApiController@editBoosterCut');
+
